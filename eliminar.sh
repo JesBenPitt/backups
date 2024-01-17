d713c0a17a1b0003841704690a165b5cd356dcff
Jesús Benítez Pitt <148717088+JesBenPitt@users.noreply.github.com>
2024-01-17 20:15:02 +0100

    Update eliminar.sh

--- eliminar.sh
+++ eliminar.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 find /home/vagrant/*.tar.gz -cmin +4 -type f -exec rm {} \;
+# Parámetro "-ctime +n" para filtrar por número de días de creación
+# Parámetro "-cmin +n" para filtrar por número de minutos de creación
